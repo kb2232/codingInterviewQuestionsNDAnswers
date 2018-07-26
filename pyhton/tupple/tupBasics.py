b34228abc7ea20ef0c77ea5118654e241f416cb8
kunle babatunde <kb2232@nyu.edu>
2018-07-26 13:03:50 -0400

    tuple basics

---
+++ pyhton/tupple/tupBasics.py
@@ -0,0 +1,12 @@
+# tuple basics
+tup = ();
+print("tup = ",tup);
+tup2 = (1,2,3);
+print ('tup2 = ',tup2);
+tup3 = (1, "hello", 'T');
+print('tup3 = ',tup3);
+tup4 = ("mouse",[1,3,4],tup2);
+print('tup34 = ',tup4);
+print(tup4[0]);
+print(tup4[1][1]);
+print(tup4[2]);
